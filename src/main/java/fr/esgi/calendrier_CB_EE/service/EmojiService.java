d4bbbe42b67c1b6e277c951aaa274408b4ed97d2
Erwan Egasse <erwan.egasse@outlook.com>
2024-07-12 18:50:24 +0200

    draft new methods to EmojiService (interface)

--- src/main/java/fr/esgi/calendrier_CB_EE/service/EmojiService.java
+++ src/main/java/fr/esgi/calendrier_CB_EE/service/EmojiService.java
@@ -1,6 +1,7 @@
 package fr.esgi.calendrier_CB_EE.service;
 
 import fr.esgi.calendrier_CB_EE.business.Emoji;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,4 +12,12 @@ public interface EmojiService {
     List<Emoji> recupererEmojis();
 
     Emoji recupererEmoji(Long idEmoji);
+
+    List<Emoji> findAll();
+
+    Emoji findById(Long id);
+
+    void save(Emoji entity);
+
+    void delete(Long id);
 }
